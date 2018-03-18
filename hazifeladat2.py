45fa4c1ad81cbe2178c4f36569075e525218603a
AAdorjan <36513630+AAdorjan@users.noreply.github.com>
2018-03-18 17:18:46 +0100

    Kiegészítés

--- hazifeladat2.py
+++ hazifeladat2.py
@@ -133,7 +133,190 @@ def feldat_11():
     print("Legrövidebb . ! ? szo hossza:", min,min_sor)
     fajl.close()
 
+def feladat_12():
+    fajl = open("be.txt", mode="r")
+    for sor in fajl:
+        sor = sor.strip()
+        fajl = open("ki2.txt", mode="w")
+        fajl.write(sor)
+        fajl.close()
 
+def osszeg2(n):
+    s = 0
+    while n:
+        s -= n % 10
+        n //= 10
+    return s
+
+def feladat_13(szam):
+    db=0
+    fajl = open("be.txt", mode="r")
+    for sor in fajl:
+        sor = sor.strip()
+        if sor >osszeg2(szam):
+            db=db+1
+
+
+def feladat_14():
+    fajl = open("be.txt", mode="r")
+    for sor in fajl:
+        sor = sor.strip()
+        fajl = open("ki2.txt", mode="w")
+        fajl.write(sor)
+        fajl.close()
+
+
+def feladat_15():
+    fajl = open("be3.txt", mode="r")
+    for sor in fajl:
+        if sor=="":
+            break
+        else:
+            fajl = open("ki1.txt", mode="w")
+            sor = sor.strip()
+            fajl.write(sor)
+            fajl.close()
+            print(sor)
+
+def feladat_16():
+    try:
+        fajl = open("be5.txt",mode="r")
+        nagy=True
+        for sor in fajl:
+            li=sor.split(" ")
+            for szo in li:
+                if(not szo[0].isupper()):
+                    nagy=False
+            if nagy:
+                fajl = open("ki2.txt", mode="w")
+                fajl.write(sor)
+                fajl.close()
+                print(sor)
+                break
+            else:
+                nagy=True
+    except Exception as e:
+        print("valamilyen kivetel dobodott",e)
+
+
+def feladat_17():
+    try:
+        fajl = open("be6.txt",mode="r")
+        kicsi=True
+        for sor in fajl:
+            li=sor.split(" ")
+            for szo in li:
+                if(not szo[0].islower()):
+                    kicsi=False
+            if kicsi:
+                fajl = open("ki3.txt", mode="w")
+                fajl.write(sor)
+                fajl.close()
+                print(sor)
+                break
+            else:
+                kicsi=True
+    except Exception as e:
+        print("valamilyen kivetel dobodott",e)
+
+def feladat_18():
+    try:
+        fajl = open("be7.txt",mode="r")
+        scoreA = 0
+        scoreB = 0
+        for sor in fajl:
+            sor = sor.strip()
+            a, b = sor.split(":")
+            print(a,b)
+            if a>b:
+                scoreA=scoreA+1
+            elif b>a:
+                scoreB=scoreB+1
+        if scoreB > scoreA:
+            print("A masodik nyert")
+        elif scoreA > scoreB:
+            print("Az elso nyert")
+    except OSError:
+        print ("hiba a fajl megnyitasa kozben")
+    except Exception:
+        pass
+
+
+def feladat_19():
+    li=[]
+    fajl = open("be8.txt", mode="r")
+    for sor in fajl:
+        sor=sor.strip()
+        a,b = sor.split(" ")
+        print(b)
+        li.append(b)
+    print(li)
+    print("Legtobb nezzetseg: ",max(li))
+
+def feladat_20():
+    li = []
+    fajl = open("be9.txt", mode="r")
+    for sor in fajl:
+        sor = sor.strip()
+        a,b,c = sor.split(";")
+        print(c)
+        li.append(c)
+    print(li)
+    kutya=max(li)
+    print("Város: ",kutya)
+
+def feladat_21():
+    fajl = open("be11.txt", mode="r")
+    for sor in fajl:
+        sor = sor.strip()
+        sor = sor.split(";")
+        print(sor)
+        #print(sum(sor))
+
+def feladat_22():
+    li = []
+    fajl = open("be10.txt", mode="r")
+    for sor in fajl:
+        sor = sor.strip()
+        a, b, c = sor.split(";")
+        print(c)
+        li.append(c)
+    print(li)
+    kutya = min(li)
+    print(kutya)
+
+def feladat_23():
+    try:
+        fajl = open("be1.txt", mode="r")
+        sor = fajl.readline()
+        sor = sor.strip()
+        kapcsolo = True
+        x1, y1= sor.split()
+        x1 = int(x1)
+        y1 = int(y1)
+        for sor in fajl:
+            sor = sor.strip()
+            x2, y2, sz2, m2 = sor.split()
+            x2 = int(x2)
+            y2 = int(y2)
+            sz2 = int(sz2)
+            m2 = int(m2)
+            if not (x2 > x1 and y2 > y1 and x2 + sz2 < x1 ):
+                kapcsolo = False
+                break
+            else:
+                x1 = x2
+                y1 = y2
+        if kapcsolo:
+            print("YES")
+        else:
+            print("NO")
+    except OSError as e:
+        print(e)
+    except Exception as e:
+        print(e)
+    finally:
+        fajl.close()
 
 def main():
     print(feladat_1(8))
@@ -149,7 +332,25 @@ def main():
     feladat_9()
     feladat_10()
     feldat_11()
-    feladat_12()
+    #feladat_12()
+    #feladat_13()
+    #feladat_14()
+    feladat_15()
+    feladat_16()
+    feladat_17()
+    feladat_18()
+    feladat_19()
+    feladat_20()
+    feladat_21()
+    feladat_22()
+    #feladat_23()
+    #print(osszeg2(28))
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
